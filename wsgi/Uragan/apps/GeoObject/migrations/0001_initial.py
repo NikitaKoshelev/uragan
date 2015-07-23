bc98d2b2c8dc56e39795ef73c309884713bd86c3
NikitaKoshelev <nikita.koshelev@gmail.com>
2015-07-23 11:10:23 +0300

    Edit TLE/update_subsat_points scripts

--- wsgi/Uragan/apps/GeoObject/migrations/0001_initial.py
+++ wsgi/Uragan/apps/GeoObject/migrations/0001_initial.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 import datetime
 from django.utils.timezone import utc
+import django.contrib.gis.db.models.fields
 from django.conf import settings
 
 
@@ -17,18 +18,19 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='GeoObject',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
                 ('title', models.TextField(unique=True, verbose_name='title geographical object')),
                 ('lat', models.FloatField(verbose_name='northern latitude in degrees')),
                 ('lon', models.FloatField(verbose_name='eastern longitude in degrees')),
                 ('short_description', models.TextField(null=True, verbose_name='short description', blank=True)),
                 ('description', models.TextField(null=True, verbose_name='description', blank=True)),
-                ('polygon', models.TextField(null=True, verbose_name='polygon in KML format', blank=True)),
-                ('color', models.CharField(null=True, max_length=20, verbose_name='color', blank=True)),
+                ('geometry', django.contrib.gis.db.models.fields.GeometryField(srid=4326, null=True, verbose_name='geometry in KML format', blank=True)),
+                ('color', models.CharField(default='#0000ff', null=True, blank=True, verbose_name='color', max_length=20)),
                 ('creation_datetime', models.DateTimeField(auto_now_add=True, verbose_name='creation date')),
                 ('last_modification', models.DateTimeField(auto_now=True, verbose_name='last modification')),
             ],
             options={
+                'ordering': ('title',),
                 'verbose_name': 'geographical object',
                 'verbose_name_plural': 'geographical objects',
             },
@@ -36,10 +38,10 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Images',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
                 ('title', models.CharField(max_length=100, verbose_name='title')),
                 ('description', models.TextField(null=True, verbose_name='description', blank=True)),
-                ('image', models.ImageField(upload_to='geo_objects/images/', verbose_name='image')),
+                ('image', models.ImageField(verbose_name='image', upload_to='geo_objects/images/')),
             ],
             options={
                 'verbose_name': 'geographical object image',
@@ -49,13 +51,16 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='SurveillancePlan',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
                 ('title', models.TextField(verbose_name='title surveillance plan')),
-                ('time_start', models.DateTimeField(auto_now_add=True)),
-                ('time_end', models.DateTimeField(default=datetime.datetime(2015, 6, 27, 0, 54, 9, 843803, tzinfo=utc))),
-                ('last_modification', models.DateTimeField(auto_now=True, verbose_name='date and time created')),
-                ('geo_objects', models.ManyToManyField(verbose_name='observed objects', to='GeoObject.GeoObject')),
-                ('researches', models.ManyToManyField(verbose_name='researches', to=settings.AUTH_USER_MODEL)),
+                ('short_description', models.TextField(null=True, verbose_name='short description', blank=True)),
+                ('description', models.TextField(null=True, verbose_name='description', blank=True)),
+                ('time_start', models.DateTimeField(auto_now=True, verbose_name='date and time start')),
+                ('time_end', models.DateTimeField(default=datetime.datetime(2015, 7, 26, 8, 0, 4, 853275, tzinfo=utc), verbose_name='date and time end')),
+                ('creation_datetime', models.DateTimeField(auto_now_add=True, verbose_name='creation date and time')),
+                ('last_modification', models.DateTimeField(auto_now=True, verbose_name='modification date and time')),
+                ('geo_objects', models.ManyToManyField(to='GeoObject.GeoObject', verbose_name='observed objects')),
+                ('researchers', models.ManyToManyField(to=settings.AUTH_USER_MODEL, verbose_name='researchers', blank=True)),
             ],
             options={
                 'verbose_name': 'surveillance plan',
@@ -65,7 +70,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='geoobject',
             name='images',
-            field=models.ManyToManyField(to='GeoObject.Images', verbose_name='images of geographical object', blank=True),
+            field=models.ManyToManyField(to='GeoObject.Images', null=True, verbose_name='images of geographical object', blank=True),
         ),
         migrations.AlterUniqueTogether(
             name='geoobject',
--- wsgi/Uragan/apps/TLE/migrations/0001_initial.py
+++ wsgi/Uragan/apps/TLE/migrations/0001_initial.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import django.contrib.gis.db.models.fields
 
 
 class Migration(migrations.Migration):
@@ -13,36 +14,54 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Satellite',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
                 ('title', models.CharField(max_length=200, verbose_name='satellite title')),
                 ('satellite_number', models.IntegerField(unique=True, verbose_name='number of satellites in the database NORAD')),
                 ('about_satellite', models.TextField(null=True, verbose_name='about satellite')),
-                ('image', models.ImageField(upload_to='satellites', null=True, verbose_name='satellite image')),
+                ('image', models.ImageField(null=True, verbose_name='satellite image', upload_to='satellites')),
             ],
             options={
                 'db_table': 'Satellites',
-                'verbose_name_plural': 'satellites',
                 'verbose_name': 'satellite',
+                'verbose_name_plural': 'satellites',
+            },
+        ),
+        migrations.CreateModel(
+            name='SubsatellitePoint',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('date_time', models.DateTimeField(verbose_name='date and time')),
+                ('location', django.contrib.gis.db.models.fields.PointField(srid=4326, verbose_name='subsatellite point location', geography=True)),
+            ],
+            options={
+                'ordering': ('-date_time',),
+                'verbose_name': 'subsatellite point',
+                'verbose_name_plural': 'subsatellite points',
             },
         ),
         migrations.CreateModel(
             name='TLE',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
-                ('datetime_created', models.DateTimeField(auto_now_add=True, verbose_name='date and time of creation')),
+                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('datetime_created', models.DateTimeField(auto_now_add=True, null=True, verbose_name='date and time of creation')),
                 ('title_line', models.CharField(max_length=24, verbose_name='title in line two-line element set')),
                 ('line1', models.CharField(max_length=69, verbose_name='first line in two-line element set')),
                 ('line2', models.CharField(max_length=69, verbose_name='second line in two-line element set')),
-                ('datetime_in_lines', models.DateTimeField(blank=True, verbose_name='date and time in two-line element set')),
+                ('datetime_in_lines', models.DateTimeField(verbose_name='date and time in two-line element set', blank=True)),
                 ('satellite', models.ForeignKey(to='TLE.Satellite', to_field='satellite_number')),
             ],
             options={
                 'db_table': 'TLE',
-                'ordering': ['-datetime_in_lines'],
-                'get_latest_by': 'datetime_in_lines',
                 'verbose_name': 'two-line element set',
                 'verbose_name_plural': 'two-line element sets',
+                'ordering': ['-datetime_in_lines'],
+                'get_latest_by': 'datetime_in_lines',
             },
+        ),
+        migrations.AddField(
+            model_name='subsatellitepoint',
+            name='tle',
+            field=models.ForeignKey(to='TLE.TLE'),
         ),
         migrations.AlterUniqueTogether(
             name='tle',
